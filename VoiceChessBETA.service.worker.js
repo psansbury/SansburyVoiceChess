6622007b373c4de831b3ad31e0095055b5fbe25c
psansbury <psansbury@lenovo.com>
2026-02-12 15:51:50 +0000

    reduced lighting
    turn fix and lighting

--- VoiceChessBETA.service.worker.js
+++ VoiceChessBETA.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770910405|12094452143';
+const CACHE_VERSION = '1770911458|13147646971';
 /** @type {string} */
 const CACHE_PREFIX = 'Wizard Chess Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
