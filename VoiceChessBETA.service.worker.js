bf01f7e4b4f0b75c5704467fce05bb9f24b5972c
psansbury <psansbury@lenovo.com>
2026-02-12 10:57:50 +0000

    Replaced files
    Fix for missing assets

--- VoiceChessBETA.service.worker.js
+++ VoiceChessBETA.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770890545|1274124824';
+const CACHE_VERSION = '1770893692|4420562967';
 /** @type {string} */
 const CACHE_PREFIX = 'Wizard Chess Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
