f9a887d926596d6cc9ec99481a7ce99b25941f1c
psansbury <psansbury@lenovo.com>
2026-02-12 11:21:02 +0000

    correcting mic icon and system permissions
    re exported to fix audio input

--- VoiceChessBETA.service.worker.js
+++ VoiceChessBETA.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770893692|4420562967';
+const CACHE_VERSION = '1770894998|5727160659';
 /** @type {string} */
 const CACHE_PREFIX = 'Wizard Chess Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
