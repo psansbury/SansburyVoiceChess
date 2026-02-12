ba8305f8250475c6683688826b866a9ac040decc
psansbury <psansbury@lenovo.com>
2026-02-12 13:19:32 +0000

    Breakfix
    Fixed load and voice hopefully!

--- VoiceChessBETA.service.worker.js
+++ VoiceChessBETA.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770896464|339867085';
+const CACHE_VERSION = '1770901907|3596205168';
 /** @type {string} */
 const CACHE_PREFIX = 'Wizard Chess Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
