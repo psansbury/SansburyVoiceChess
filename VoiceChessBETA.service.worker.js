ad7b7f7f80939bcfcccc691c6bd393c46d2855fd
psansbury <psansbury@lenovo.com>
2026-02-12 19:05:57 +0000

    Menu fix

--- VoiceChessBETA.service.worker.js
+++ VoiceChessBETA.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770918944|20632776505';
+const CACHE_VERSION = '1770923104|24793180687';
 /** @type {string} */
 const CACHE_PREFIX = 'Wizard Chess Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
